d4c1207664e889fa71989807af6b3bd48504f35e
Pedro Henrique Bergamo <pedroh.bergamo20@gmail.com>
2025-04-06 21:52:00 -0300

    adiciona configuração de utilitários e ajustes na estrutura do projeto

--- web/app/routes/home.tsx
+++ web/app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta(_: Route.MetaArgs) {
+	return [{ title: "Início | Fashion Stock" }];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return (
+		<main>
+			<h1>Fashion Stock</h1>
+		</main>
+	);
 }
